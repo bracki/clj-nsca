610f742307459c89c754ad3f6575fa620dbc8b9e
Jan Brauer <jan@jimdo.com>
2013-09-24 21:57:36 +0200

    Add :url

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject clj-nsca "0.0.1-SNAPSHOT"
   :description "Clojure wrapper for jsendnsca - Send passive Nagios checks from Clojure."
+  :url "http://github.com/bracki/clj-nsca"
   :repositories {"local" "file:mvn-repo"}
   :main clj-nsca.core
   :dependencies [[org.clojure/clojure "1.5.1"]
