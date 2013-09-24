3e4cf26785d3ea5141b2e276f0eeb2923b2940f8
Jan Brauer <jan@jimdo.com>
2013-09-24 23:18:30 +0200

    No more snapshots

--- project.clj
+++ project.clj
@@ -1,8 +1,11 @@
-(defproject clj-nsca "0.0.1-SNAPSHOT"
+(defproject clj-nsca "0.0.1"
   :description "Clojure wrapper for jsendnsca - Send passive Nagios checks from Clojure."
   :url "http://github.com/bracki/clj-nsca"
-  :repositories {"local" "file:mvn-repo"}
   :main clj-nsca.core
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.googlecode.jsendnsca/jsendnsca "2.1-SNAPSHOT"]]
+                 [com.googlecode.jsendnsca/jsendnsca "2.1"]]
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "same as Clojure"}
   :profiles {:dev {:dependencies [[midje "1.5.0"]]}})
